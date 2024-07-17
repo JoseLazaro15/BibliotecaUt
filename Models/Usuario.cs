dc01dc6ed96bd224fcd97124c644d40930a0c081
anzures22 <anzu_anzu22@hotmail.com>
2024-07-16 21:39:31 -0600

    Actualizar base de datos
    Actualize la base de datos, elimine lineas de codigo en BiBliotecaContex y agregue las clases de las nuevas tablas.

---
+++ Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticaBiblioteca.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Contrasena { get; set; }
+
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+
+        // Relación con Rol
+        public int IdRol { get; set; }
+
+        [ForeignKey("IdRol")]
+        public Rol Rol { get; set; }
+
+    }
+}

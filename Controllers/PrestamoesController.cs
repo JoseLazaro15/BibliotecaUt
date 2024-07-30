b1c69febfc3d90262748b8bb3bce06aadb1341e6
Jose Luis Lazaro <jl.lazaro15@gmail.com>
2024-07-29 21:37:28 -0600

    nnew commit

--- Controllers/PrestamoesController.cs
+++ Controllers/PrestamoesController.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using iText.Kernel.Pdf;
-using iText.Layout;
-using iText.Layout.Element;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -21,106 +18,12 @@ namespace PracticaBiblioteca.Controllers
             _context = context;
         }
 
-        // Método para generar el PDF
-        public async Task<IActionResult> ImprimirSimple()
-        {
-            var prestamos = await _context.Prestamos.Include(p => p.IdEstadoPrestamoNavigation)
-                                                    .Include(p => p.IdLibroNavigation)
-                                                    .Include(p => p.IdPersonaNavigation)
-                                                    .ToListAsync();
-
-            using (var ms = new MemoryStream())
-            {
-                var writer = new PdfWriter(ms);
-                var pdf = new PdfDocument(writer);
-                var document = new Document(pdf);
-
-                // Agrega el encabezado
-                var header = new Paragraph("UNIVERSIDAD TECNOLOGICA DE LA REGION NORTE DEL ESTADO DE GUERRERO")
-                    .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
-                    .SetFontSize(18);
-                document.Add(header);
-
-                var subheader = new Paragraph("PRESTAMO DE LIBROS BIBLIOTECA UT")
-                    .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
-                    .SetFontSize(14);
-                document.Add(subheader);
-
-                var subsubheader = new Paragraph("La imaginación te llevará a cualquier parte")
-                    .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
-                    .SetFontSize(12);
-                document.Add(subsubheader);
-
-                // Agrega un espacio
-                document.Add(new Paragraph(" "));
-
-                // Agrega la tabla de información
-                foreach (var prestamo in prestamos)
-                {
-                    var table = new Table(2);
-                    table.AddCell(new Cell().Add(new Paragraph("FECHA")));
-                    table.AddCell(new Cell().Add(new Paragraph(DateTime.Now.ToString("dd/MM/yyyy")))); // Fecha actual
-
-                    table.AddCell(new Cell().Add(new Paragraph("NOMBRE")));
-                    table.AddCell(new Cell().Add(new Paragraph(prestamo.IdPersonaNavigation.Nombre)));
-
-                    table.AddCell(new Cell().Add(new Paragraph("TELEFONO")));
-                    table.AddCell(new Cell().Add(new Paragraph("N/A"))); // Puedes agregar el campo de teléfono si está disponible
-
-                    table.AddCell(new Cell().Add(new Paragraph("CLAVE")));
-                    table.AddCell(new Cell().Add(new Paragraph("N/A"))); // Puedes agregar el campo de clave si está disponible
-
-                    table.AddCell(new Cell().Add(new Paragraph("CUATRIMESTRE")));
-                    table.AddCell(new Cell().Add(new Paragraph("N/A"))); // Puedes agregar el campo de cuatrimestre si está disponible
-
-                    table.AddCell(new Cell().Add(new Paragraph("GRUPO")));
-                    table.AddCell(new Cell().Add(new Paragraph("N/A"))); // Puedes agregar el campo de grupo si está disponible
-
-                    table.AddCell(new Cell().Add(new Paragraph("DIAS")));
-                    table.AddCell(new Cell().Add(new Paragraph("N/A"))); // Puedes agregar el campo de días si está disponible
-
-                    table.AddCell(new Cell().Add(new Paragraph("TITULO")));
-                    table.AddCell(new Cell().Add(new Paragraph(prestamo.IdLibroNavigation.Titulo)));
-
-                    table.AddCell(new Cell().Add(new Paragraph("CODIGO")));
-                    table.AddCell(new Cell().Add(new Paragraph("N/A"))); // Puedes agregar el campo de código si está disponible
-
-
-                    document.Add(table);
-
-                    // Agrega un espacio entre registros
-                    document.Add(new Paragraph(" "));
-                }
-
-                // Agrega un párrafo final
-                var footer = new Paragraph("El libro que estoy solicitando a la biblioteca de esta institución se encuentran en buen estado, las fechas de préstamo y de entrega se encuentran indicadas en la parte superior, me comprometo a cumplirlas y evitar las sanciones correspondientes. En caso de maltrato o extravío del libro aquí mencionado tendré que cubrir la totalidad del costo para que sea repuesto. Costo por día de retraso $3.00 pesos.")
-                    .SetTextAlignment(iText.Layout.Properties.TextAlignment.JUSTIFIED)
-                    .SetFontSize(10);
-                document.Add(footer);
-
-                // Agrega un espacio y la firma
-                document.Add(new Paragraph(" "));
-                document.Add(new Paragraph(" "));
-                var firma = new Paragraph("FIRMA DEL ALUMNO")
-                    .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
-                    .SetFontSize(12);
-                document.Add(firma);
-
-                document.Close();
-                var byteInfo = ms.ToArray();
-
-                return File(byteInfo, "application/pdf", "Prestamos.pdf");
-            }
-        }
-
-
         // GET: Prestamoes
         public async Task<IActionResult> Index()
         {
             var bibliotecaContext = _context.Prestamos.Include(p => p.IdEstadoPrestamoNavigation).Include(p => p.IdLibroNavigation).Include(p => p.IdPersonaNavigation);
             return View(await bibliotecaContext.ToListAsync());
         }
-
 
         // GET: Prestamoes/Details/5
         public async Task<IActionResult> Details(int? id)

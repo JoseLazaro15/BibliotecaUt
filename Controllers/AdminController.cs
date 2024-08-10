f61473687b175617f4c0bb5bd3245b60cb33d3a1
Jose Luis Lazaro <jl.lazaro15@gmail.com>
2024-08-10 16:43:35 -0600

    makin a revert

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticaBiblioteca.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        // Otros métodos y acciones para el Admin
+    }
+}
